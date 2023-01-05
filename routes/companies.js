268f3a23443864654ff4e81e24d686e0f6ef6753
Ali Cetiner <ayenercetiner@gmail.com>
2023-01-04 23:26:27 -0600

    company routes

---
+++ routes/companies.js
@@ -0,0 +1,82 @@
+const express = require("express");
+
+const db = require("../db");
+const ExpressError = require("../expressError");
+
+const router = new express.Router();
+
+router.get("/", async function (req, res, next) {
+  try {
+    const results = await db.query(`SELECT code, name FROM companies`);
+
+    return res.json({ companies: results.rows });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.get("/:code", async function (req, res, next) {
+  try {
+    let code = req.params.code;
+    const results = await db.query(
+      `SELECT code, name, description FROM companies WHERE code=$1`,
+      [code]
+    );
+    if (results.rows.length === 0) {
+      throw new ExpressError(`Company not found with code ${code}`, 404);
+    }
+    return res.json({ company: results.rows[0] });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.post("/", async function (req, res, next) {
+  try {
+    const { code, name, description } = req.body;
+    if (code.trim() === "" || name.trim() === "") {
+      throw new ExpressError(
+        `Required data is missing! Check company code and name`,
+        404
+      );
+    }
+    const results = await db.query(
+      `INSERT INTO companies (code, name, description) VALUES($1, $2, $3) RETURNING code, name, description`,
+      [code, name, description]
+    );
+    if (results.rows.length === 0) {
+      throw new ExpressError(
+        `Required data is missing! Check company code and name`,
+        404
+      );
+    }
+    return res.json({ company: results.rows[0] });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.put("/:code", async function (req, res, next) {
+  try {
+    const { name, description } = req.body;
+    const code = req.params.code;
+    if (name.trim() === "") {
+      throw new ExpressError(
+        `Required data is missing! Check company name`,
+        404
+      );
+    }
+    const results = await db.query(
+      `UPDATE companies SET name = $1, description = $2 WHERE  code = $3 RETURNING code, name, description`,
+      [name, description, code]
+    );
+    if (results.rows.length === 0) {
+      throw new ExpressError(`Company not found`, 404);
+    }
+    return res.json({ company: results.rows[0] });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
